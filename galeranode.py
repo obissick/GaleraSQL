d013a310c9123d56984e25f1839611d292f8af0e
Oren Bissick <obissick@greathealthworks.com>
2018-01-16 19:10:47 -0500

    add galera status.

--- galeranode.py
+++ galeranode.py
@@ -13,21 +13,95 @@ class GaleraNode:
     port = ""
     username = ""
     password = ""
-    
+    state = []
+    s_queue = []
+    s_queue_avg = []
+    r_queue = []
+    r_queue_avg = []
+    sent = []
+    received = []
+    paused = []
+    committed = []
+
     def __init__(self, host, port, username, password):
         self.host = host
         self.port = port
         self.username = username
         self.password = password
-        
-    def get_status(self):
-        result = self.query_node("SHOW STATUS LIKE 'wsrep_local_state_comment';")
-        return result[1]
+
+    def set_state(self):
+        self.state = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_local_state_comment';")
+
+    def set_squeue(self):
+        self.s_queue = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_local_send_queue';")
+
+    def set_squeue_avg(self):
+        self.s_queue_avg = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_local_send_queue_avg';")
+
+    def set_rqueue(self):
+        self.r_queue = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_local_recv_queue';")
+
+    def set_rqueue_avg(self):
+        self.r_queue_avg = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_local_recv_queue_avg';")
+    
+    def set_fsent(self):
+        self.sent = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_flow_control_sent';")
+
+    def set_freceived(self):
+        self.received = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_flow_control_recv';")
+    
+    def set_fpaused(self):
+        self.paused = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_flow_control_paused';")
+
+    def set_committed(self):
+        self.committed = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_last_committed';")
+
+    def get_status(self): 
+        t1 = Thread(target=self.set_state)
+        t2 = Thread(target=self.set_squeue)
+        t3 = Thread(target=self.set_squeue_avg)
+        t4 = Thread(target=self.set_rqueue)
+        t5 = Thread(target=self.set_rqueue_avg)
+        t6 = Thread(target=self.set_fsent)
+        t7 = Thread(target=self.set_freceived)
+        t8 = Thread(target=self.set_fpaused)
+        t9 = Thread(target=self.set_committed)
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+        t5.start()
+        t6.start()
+        t7.start()
+        t8.start()
+        t9.start()
+        t1.join()
+        t2.join()
+        t3.join()
+        t4.join()
+        t5.join()
+        t6.join()
+        t7.join()
+        t8.join()
+        t9.join()
+        result = [self.state[1], self.s_queue[1] + "/" + self.s_queue_avg[1],
+            self.r_queue[1] + "/" + self.r_queue_avg[1], self.sent[1], self.received[1], self.paused[1], self.committed[1]]
+        return result
     
     def get_version(self):
-        result = self.query_node("SELECT @@version;")
-        return result[0]
-    
+        result = self.query_node(
+            "SHOW GLOBAL STATUS LIKE 'wsrep_provider_version';")
+        return result[1]
+
     def get_hostname(self):
         return self.host
     
--- gsql.py
+++ gsql.py
@@ -2,14 +2,13 @@
 import subprocess
 import sys
 import argparse
-from tabulate import tabulate
 import logging
-from galeranode import GaleraNode
 from threading import Thread
-import mysql.connector
-from mysql.connector import Error
+from tabulate import tabulate
+from galeranode import GaleraNode
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s (%(threadName)-2s) %(message)s',)
 logger = logging.getLogger(__name__)
 
 def run():
@@ -41,14 +40,17 @@ def run():
             logger.info('Checking if query is compatible.')
             for node in gnodes:
                 logger.info('Running query on node %s', node.host)
-                node.query(args.query)            
+                node.query_node(args.query)            
         elif args.query.lower() == "show galera status":
             for i in range(len(gnodes)):
                 statuses.append([])
+                node_status = gnodes[i].get_status()
                 statuses[i].append(gnodes[i].get_hostname())
-                statuses[i].append(gnodes[i].get_status())
+                for ii in range(len(node_status)):
+                    statuses[i].append(node_status[ii])   
             statuses.sort()
-            print_out(statuses, ["Host", "Status"])
+            print_out(statuses, ["Host", "Status", "Send Q [Now/Avg]",
+                                 "Receive Q [Now/Avg]", "Flow Ctrl Sent", "Flow Ctrl Recieved", "Flow Ctrl Paused", "Last Committed"])
         
         elif args.query.lower() == "show galera version":
             for i in range(len(gnodes)):
@@ -56,7 +58,15 @@ def run():
                 statuses[i].append(gnodes[i].get_hostname())
                 statuses[i].append(gnodes[i].get_version())
             statuses.sort()
-            print_out(statuses, ["Host", "DB Version"])
+            print_out(statuses, ["Host", "Galera Version"])
+
+        elif args.query.lower() == "show galera flow":
+            for i in range(len(gnodes)):
+                statuses.append([])
+                statuses[i].append(gnodes[i].get_hostname())
+                statuses[i].append(gnodes[i].get_flow())
+            statuses.sort()
+            print_out(statuses, ["Host", "Sent, Received, Paused"])
         else:
             logger.error(
                 'This Query should not be ran on all nodes at the same time.')
